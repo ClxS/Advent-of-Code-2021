0213a37bc8c14042402c996c00ddcef1d3cc3e51
Carl <slxxls92@gmail.com>
2021-12-01 14:10:39 +0000

    Benchmark.NET Integration

---
+++ AdventOfCode/Benchmarking/BenchmarkLatest.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode.Benchmarking
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using AoCHelper;
+    using BenchmarkDotNet.Attributes;
+
+    public class BenchmarkLatest
+    {
+        private readonly BaseProblem problem;
+        
+        public BenchmarkLatest()
+        {
+            var solver = Assembly
+                .GetExecutingAssembly()
+                .GetTypes()
+                .Last(type => typeof(BaseProblem).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract);
+            
+            this.problem = (BaseProblem)Activator.CreateInstance(solver)!;
+        }
+        
+        [Benchmark]
+        public async ValueTask<string> Part1()
+        {
+            return await this.problem.Solve_1();
+        }
+        
+        [Benchmark]
+        public async ValueTask<string> Part2()
+        {
+            return await this.problem.Solve_1();
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Linq;
+using AdventOfCode.Benchmarking;
 using AoCHelper;
+using BenchmarkDotNet.Running;
 
 if (args.Length == 0)
 {
     Solver.SolveLast();
+    #if !DEBUG
+    BenchmarkRunner.Run<BenchmarkLatest>();
+    #endif
 }
 else if (args.Length == 1 && args[0].Contains("all", StringComparison.CurrentCultureIgnoreCase))
 {
