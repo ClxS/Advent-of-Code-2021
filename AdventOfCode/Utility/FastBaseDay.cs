4d03dd5568b787a10eff88cd1efcee345e5f948f
Carl <slxxls92@gmail.com>
2021-12-10 04:45:16 +0000

    The base class now reads the string for us

--- AdventOfCode/Utility/FastBaseDay.cs
+++ AdventOfCode/Utility/FastBaseDay.cs
@@ -1,10 +1,18 @@
 ﻿namespace AdventOfCode.Utility
 {
+    using System.IO;
     using System.Threading.Tasks;
     using AoCHelper;
 
     public abstract class FastBaseDay<T> : BaseDay, IFastDay
     {
+        public FastBaseDay()
+        {
+            this.Input = File.ReadAllText(this.InputFilePath).Replace("\r", "");
+        }
+
+        protected string Input { get; }
+
         public override ValueTask<string> Solve_1()
         {
             var result = this.Solve1();
