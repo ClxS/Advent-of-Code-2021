4f31b2767d5eaf68d43e122a0e42669aff008516
Carl <slxxls92@gmail.com>
2021-12-10 04:45:06 +0000

    Setup day 10

---
+++ AdventOfCode/Day10.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode
+{
+    using System.IO;
+    using Utility;
+
+    // ReSharper disable once UnusedType.Global
+    public sealed class Day10 : FastBaseDay<int>
+    {
+        protected override int Solve1()
+        {
+            return 0;
+        }
+
+        protected override int Solve2()
+        {
+            return 0;
+        }
+    }
+}
