81aa6e93a41cc7b9ef023d61bf8fb00579f34c82
Carl <slxxls92@gmail.com>
2021-12-13 09:22:03 +0000

    Day 11

---
+++ AdventOfCode/Day11.cs
@@ -0,0 +1,132 @@
+﻿namespace AdventOfCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using Microsoft.Toolkit.HighPerformance;
+    using Utility;
+
+    // ReSharper disable once UnusedType.Global
+    public sealed unsafe class Day11 : FastBaseDay<int>
+    {
+        protected override int Solve1()
+        {
+            var flashCount = 0;
+            fixed (char* ptr = this.InputString)
+            {
+                var map = new Span2D<char>(ptr, 10, 10, 1);
+                for (var step = 0; step < 100; step++)
+                {
+                    for (var y = 0; y < 10; y++)
+                    {
+                        for (var x = 0; x < 10; x++)
+                        {
+                            map[y, x]++;
+                            this.CheckForFlashes(map, x, y, ref flashCount);
+                        }
+                    }
+                    
+                    for (var y = 0; y < 10; y++)
+                    {
+                        for (var x = 0; x < 10; x++)
+                        {
+                            if (map[y, x] < '0')
+                            {
+                                map[y, x] = '0';
+                            }
+                        }
+                    }
+                }
+            }
+
+            return flashCount;
+        }
+
+        protected override int Solve2()
+        {
+            var flashCount = 0;
+            fixed (char* ptr = this.InputString)
+            {
+                var map = new Span2D<char>(ptr, 10, 10, 1);
+                var step = 1;
+                while(true)
+                {
+                    for (var y = 0; y < 10; y++)
+                    {
+                        for (var x = 0; x < 10; x++)
+                        {
+                            map[y, x]++;
+                            this.CheckForFlashes(map, x, y, ref flashCount);
+                        }
+                    }
+
+                    flashCount = 0;
+                    for (var y = 0; y < 10; y++)
+                    {
+                        for (var x = 0; x < 10; x++)
+                        {
+                            if (map[y, x] < '0')
+                            {
+                                map[y, x] = '0';
+                                flashCount++;
+                            }
+                        }
+                    }
+
+                    if (flashCount == 100)
+                    {
+                        return step;
+                    }
+                    
+                    step++;
+                }
+            }
+
+            return flashCount;
+        }
+
+        private void CheckForFlashes(Span2D<char> map, int x, int y, ref int flashCount)
+        {
+            if (map[y, x] <= '9')
+            {
+                return;
+            }
+
+            map[y, x] = '\0';
+            flashCount++;
+            for (var checkY = y - 1; checkY <= y + 1; checkY++)
+            {
+                if (checkY < 0 || checkY >= map.Height)
+                {
+                    continue;
+                }
+                
+                for (var checkX = x - 1; checkX <= x + 1; checkX++)
+                {
+                    if (checkX < 0 || checkX >= map.Width)
+                    {
+                        continue;
+                    }
+                    
+                    map[checkY, checkX]++;
+                    this.CheckForFlashes(map, checkX, checkY, ref flashCount);
+                }
+            }
+        }
+
+        private void PrintMap(Span2D<char> map)
+        {
+            for (var y = 0; y < 10; y++)
+            {
+                for (var x = 0; x < 10; x++)
+                {
+                    Console.Write(map[y, x]);
+                }
+                
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine();
+        }
+    }
+}
--- AdventOfCode/Utility/FastBaseDay.cs
+++ AdventOfCode/Utility/FastBaseDay.cs
@@ -7,34 +7,43 @@
 
     public abstract class FastBaseDay<T> : BaseDay, IFastDay
     {
+        private string input1;
+        private string input2;
         private string input;
         
         public FastBaseDay()
         {
-            this.input = File.ReadAllText(this.InputFilePath).Replace("\r", "");
+            this.input1 = File.ReadAllText(this.InputFilePath).Replace("\r", "");
+            this.input2 = File.ReadAllText(this.InputFilePath).Replace("\r", "");
         }
+
+        protected string InputString => this.input;
 
         protected ReadOnlySpan<char> Input => this.input;
 
         public override ValueTask<string> Solve_1()
         {
+            this.input = this.input1;
             var result = this.Solve1();
             return new(result?.ToString() ?? "");
         }
 
         public override ValueTask<string> Solve_2()
         {
+            this.input = this.input2;
             var result = this.Solve2();
             return new(result?.ToString() ?? "");
         }
 
         public void NonReturnSolve1()
         {
+            this.input = this.input1;
             this.Solve1();
         }
 
         public void NonReturnSolve2()
         {
+            this.input = this.input2;
             this.Solve2();
         }
 
